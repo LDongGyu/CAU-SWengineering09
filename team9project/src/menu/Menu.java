2491377cd94b93518901c0578de7235f3bead35f
LDongGyu <ehdrbchd@naver.com>
2018-05-26 12:38:37 +0900

    LOAD추가

--- team9project/src/menu/Menu.java
+++ team9project/src/menu/Menu.java
@@ -1,14 +1,17 @@
 package menu;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
-public class Menu implements ActionListener{
+public class Menu {
 	//Menu class콜했을시 기본 틀과 패널과 버튼들
 	JFrame f = new JFrame("Comparing Text");
 	JButton Compare = new JButton("Compare");
-	JButton Merge = new JButton("Merge");
+	JButton LeftMerge = new JButton("LeftMerge");
+	JButton RightMerge = new JButton("RightMerge");
 	JButton EXIT = new JButton("EXIT");
 	
 	JButton LeftLoad = new JButton("Load");
@@ -21,6 +24,14 @@ public class Menu implements ActionListener{
 	JButton RightSave = new JButton("Save");
 	JPanel RightPanel = new JPanel();
 	
+	//TextArea를 50행, 50열로 설정합니다.      
+	JTextArea Righttextfield = new JTextArea("", 50, 50); 		
+	JTextArea Lefttextfield = new JTextArea("", 50, 50); 
+	
+	//EDIT수정 가능 불가능 용 boolean
+	private boolean LeftEditonoff = false;
+	private boolean RightEditonoff = false;
+
 	
 	public Menu(){
 		f.setSize(900,600);//화면의 크기를 구함
@@ -28,7 +39,7 @@ public class Menu implements ActionListener{
 		
 		//오른쪽 Compare & Merge 부분
 		JPanel menueastPanel = new JPanel();
-		menueastPanel.setLayout(new GridLayout(3,1,4,20));
+		menueastPanel.setLayout(new GridLayout(4,1,4,20));
 		f.add("East", menueastPanel);
 		/*버튼색깔설정할려면 여기서 색깔조정
 		Compare.setBackground(Color.pink);
@@ -36,7 +47,8 @@ public class Menu implements ActionListener{
 		EXIT.setBackground(Color.pink);
 		*/
 		menueastPanel.add("East",Compare);
-		menueastPanel.add("East",Merge);
+		menueastPanel.add("East",LeftMerge);
+		menueastPanel.add("East",RightMerge);
 		menueastPanel.add("East",EXIT);
 
 		//중앙 왼쪽 TextView 부분
@@ -53,8 +65,8 @@ public class Menu implements ActionListener{
 		LeftNorthPanel.add("North",LeftEdit);
 		LeftNorthPanel.add("North",LeftSave);
 		
-		//TextArea를 50행, 50열로 설정합니다.      
-		JTextArea Lefttextfield = new JTextArea("", 50, 50); 
+		//TextArea를 왼쪽에 추가, textfield는 기본 false로 잠겨있는상태    
+		Lefttextfield.setEditable(LeftEditonoff);
 		LeftPanel.add("Center",new JScrollPane(Lefttextfield));
 
 		
@@ -72,8 +84,8 @@ public class Menu implements ActionListener{
 		RightNorthPanel.add("North",RightEdit);
 		RightNorthPanel.add("North",RightSave);
 		
-		//TextArea를 50행, 50열로 설정합니다.      
-		JTextArea Righttextfield = new JTextArea("", 50, 50); 
+		//TextArea를 오른쪽에 추가, textfield는 기본 false로 잠겨있는상태      
+		Righttextfield.setEditable(RightEditonoff);
 		RightPanel.add("Center",new JScrollPane(Righttextfield));
 
 		
@@ -88,43 +100,114 @@ public class Menu implements ActionListener{
 		Toolkit tk = Toolkit.getDefaultToolkit(); //구현된 Toolkit객체를 얻는다.
 		Dimension screenSize = tk.getScreenSize();
 		
+		//버튼을 눌렀을시 아래에 class에 정의한내용대로 실행하도록 내용추가
+		ButtonListener listener = new ButtonListener();
+		Compare.addActionListener(listener);
+		LeftMerge.addActionListener(listener);
+		RightMerge.addActionListener(listener);
+		EXIT.addActionListener(listener);
+		
+		LeftLoad.addActionListener(listener);
+		LeftEdit.addActionListener(listener);
+		LeftSave.addActionListener(listener);
+
+		RightLoad.addActionListener(listener);
+		RightEdit.addActionListener(listener);
+		RightSave.addActionListener(listener);
+		
+		//처음 시작시 프로그램 위치 및 사이즈 설정
 		f.setLocation(screenSize.width/2 - 450, screenSize.height/2 - 300);
+
+		//오른쪽위 닫기버튼 실행시 프로그램종료되도록설정
 		f.addWindowListener(new EventHandler());
-		f.setVisible(true);//생성한 Frame을 화면에 보이도록 한다.
+		
+		//생성한 Frame을 화면에 보이도록 한다.
+		f.setVisible(true);
 	}
-	
-	public void actionPerformed(ActionEvent e){
-		if(e.getSource() == Merge){
-			//Merge관련 action시 실행될것들 내용추가
+	public class ButtonListener implements ActionListener{
+		
+		ArrayList<String> leftTXT;  // 왼쪽 파일
+		ArrayList<String> rightTXT; // 오른쪽 파일
+		String leftTXT_Directory = new String(); // 왼쪽 파일 디렉토리
+		String rightTXT_Directory = new String(); // 오른쪽 파일 디렉토리
+		
+		public void actionPerformed(ActionEvent e){
+			if(e.getSource() == LeftMerge){
+				//Merge관련 action시 실행될것들 내용추가
+			}
+			else if(e.getSource() == RightMerge){
+				//Merge관련 action시 실행될것들 내용추가
+			}
+			else if(e.getSource() == Compare){
+				//Compare관련 action시 실행될것들 내용추가
+			}
+			else if(e.getSource() == LeftLoad){
+				//Load관련 action시 실행될것들 내용추가
+				FileLoader load = new FileLoader(); // 탐색기
+				leftTXT = load.fileRead(); // 파일 가져오기
+				leftTXT_Directory = load.getDirectory(); // 경로 저장
+								
+				Lefttextfield.setText(""); // 텍스트필드 초기화 후 출력
+				for(int i = 0; i < leftTXT.size(); i++) { // 텍스트필드에 출력
+					Lefttextfield.append(leftTXT.get(i)+"\n");
+				}
+			}
+			else if(e.getSource() == LeftEdit){
+				//Edit관련 action시 실행될것들 내용추가
+				if(LeftEditonoff == false){
+					LeftEdit.setText("EDIT ON");
+					LeftEdit.setFont(new Font("고딕",Font.ITALIC,12));
+					LeftEditonoff = true;
+				}
+				else if(LeftEditonoff = true){
+					LeftEdit.setText("Edit");
+					LeftEdit.setFont(new Font("Dialog",Font.BOLD,12));
+					LeftEditonoff = false;
+				}
+				Lefttextfield.setEditable(LeftEditonoff);
+			}
+			else if(e.getSource() == LeftSave){
+				//Save관련 action시 실행될것들 내용추가
+			}
+			else if(e.getSource() == RightLoad){
+				//Load관련 action시 실행될것들 내용추가
+				FileLoader load = new FileLoader(); // 탐색기
+				rightTXT = load.fileRead(); // 파일 가져오기
+				rightTXT_Directory = load.getDirectory(); // 경로 저장
+				
+				Righttextfield.setText("");
+				for(int i = 0; i < rightTXT.size(); i++) { // 텍스트필드에 저장
+					Righttextfield.append(rightTXT.get(i)+"\n");
+				}
+			}
+			else if(e.getSource() == RightEdit){
+				//Edit관련 action시 실행될것들 내용추가
+				if(RightEditonoff == false){
+					RightEdit.setText("EDIT ON");
+					RightEdit.setFont(new Font("고딕",Font.ITALIC,12));
+					RightEditonoff = true;
+				}
+				else if(RightEditonoff = true){
+					RightEdit.setText("Edit");
+					RightEdit.setFont(new Font("Dialog",Font.BOLD,12));
+					RightEditonoff = false;
+				}
+				Righttextfield.setEditable(RightEditonoff);
+			}
+			else if(e.getSource() == RightSave){
+				//Save관련 action시 실행될것들 내용추가
+			}
+			else if(e.getSource() == EXIT){
+				f.setVisible(false);
+				f.dispose();
+				System.exit(0);
+			}
 		}
-		else if(e.getSource() == Compare){
-			//Compare관련 action시 실행될것들 내용추가
-		}
-		else if(e.getSource() == LeftLoad){
-			//Load관련 action시 실행될것들 내용추가
-		}
-		else if(e.getSource() == LeftEdit){
-			//Edit관련 action시 실행될것들 내용추가
-		}
-		else if(e.getSource() == LeftSave){
-			//Save관련 action시 실행될것들 내용추가
-		}
-		else if(e.getSource() == RightLoad){
-			//Load관련 action시 실행될것들 내용추가
-		}
-		else if(e.getSource() == RightEdit){
-			//Edit관련 action시 실행될것들 내용추가
-		}
-		else if(e.getSource() == RightSave){
-			//Save관련 action시 실행될것들 내용추가
-		}
-		else if(e.getSource() == EXIT){
-			f.setVisible(false);
-			f.dispose();
-			System.exit(0);
+		
+		public void fetchTXT(ArrayList<String>txt) {
+			
 		}
 	}
-	
 	
 }
 
